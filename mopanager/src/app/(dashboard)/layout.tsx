6439da674ca5d4aafd22c44f124b204337689d53
MopchoNex <mopchowork@gmail.com>
2023-06-03 12:55:15 +0300

    Home page implementation

--- mopanager/src/app/(dashboard)/layout.tsx
+++ mopanager/src/app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
+import '@/styles/global.css';
 import GlassPane from "@/components/GlassPane/GlassPane";
+import Sidebar from '@/components/Sidebar/Sidebar';
 
 export default function DashboardLayout({
     children,
@@ -9,7 +11,8 @@ export default function DashboardLayout({
       <html lang="en">
       <head />
       <body className="h-screen w-screen rainbow-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center">
+        <GlassPane className="w-full h-full flex items-center">
+          <Sidebar />
           {children}
         </GlassPane>
       </body>
---
+++ mopanager/src/components/Greeting/Greeting.tsx
@@ -0,0 +1,33 @@
+import { getUserFromCookie } from "@/lib/auth";
+import { cookies } from "next/headers";
+import { Button } from "../Buttons/Button";
+import Card from "../Card/Card";
+import { delay } from "@/lib/async";
+
+const getData = async () => {
+  await delay(5000);
+  const user = await getUserFromCookie(cookies());
+  return user;
+};
+
+const Greetings = async () => {
+  const user = await getData();
+
+  return (
+    <Card className="w-full py-4 relative">
+      <div className="mb-4">
+        <h1 className="text-3xl text-gray-700 font-bold mb-4">
+          Hello, {user ? user.firstName : 'User'}!
+        </h1>
+        <h4 className="text-xl text-gray-400">
+          Check your daily tasks and schedule
+        </h4>
+      </div>
+      <div>
+        <Button size="large">Today&apos;s Schedule</Button>
+      </div>
+    </Card>
+  );
+};
+
+export default Greetings;
